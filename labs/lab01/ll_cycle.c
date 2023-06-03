a1dbc0ae33094955bf2ac33d867254f3dc8d43ee
CZX1112 <1952650@tongji.edu.cn>
2023-06-03 21:35:43 +0800

    update lab01

--- labs/lab01/ll_cycle.c
+++ labs/lab01/ll_cycle.c
@@ -3,5 +3,20 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *fast = head;
+    node *slow = head;
+    while (fast != NULL) {
+        fast = fast->next;
+        slow = slow->next;
+        if (fast == NULL) {
+            return 0;
+        }
+        else {
+            fast = fast->next;
+        }
+        if (fast == slow) {
+            return 1;
+        }
+    }
     return 0;
 }
