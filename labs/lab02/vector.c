b894fbec61b015e351748979462f0ceb88f3ec06
CZX1112 <1952650@tongji.edu.cn>
2023-06-12 00:07:27 +0800

    finish task4 and prepare for task5

--- labs/lab02/vector.c
+++ labs/lab02/vector.c
@@ -13,12 +13,14 @@ struct vector_t {
 
 /* Utility function to handle allocation failures. In this
    case we print a message and exit. */
+/*处理分配失败的效用函数。 在这种情况下，我们打印一条消息并退出。*/
 static void allocation_failed() {
     fprintf(stderr, "Out of memory.\n");
     exit(1);
 }
 
 /* Bad example of how to create a new vector */
+/*如何创建新向量的错误示例*/
 vector_t *bad_vector_new() {
     /* Create the vector and a pointer to it */
     vector_t *retval, v;
@@ -36,6 +38,7 @@ vector_t *bad_vector_new() {
 }
 
 /* Another suboptimal way of creating a vector */
+/* 另一种创建向量的次优方法 */
 vector_t also_bad_vector_new() {
     /* Create the vector */
     vector_t v;
@@ -53,14 +56,19 @@ vector_t also_bad_vector_new() {
 /* Create a new vector with a size (length) of 1
    and set its single component to zero... the
    RIGHT WAY */
+/* 创建一个大小（长度）为 1 的新向量并将其单个分量设置为零
+......正确的方法 */
 vector_t *vector_new() {
     /* Declare what this function will return */
+    /* 声明此函数将返回什么 */
     vector_t *retval;
 
+    /* First, we need to allocate memory on the heap for the struct */
     /* First, we need to allocate memory on the heap for the struct */
     retval = /* YOUR CODE HERE */
 
     /* Check our return value to make sure we got memory */
+    /* 检查我们的返回值以确保我们有内存 */
     if (/* YOUR CODE HERE */) {
         allocation_failed();
     }
@@ -68,16 +76,20 @@ vector_t *vector_new() {
     /* Now we need to initialize our data.
        Since retval->data should be able to dynamically grow,
        what do you need to do? */
+    /* 现在我们需要初始化我们的数据。 既然 retval->data 应该是
+    可以动态增长的，那么需要做什么呢？ */
     retval->size = /* YOUR CODE HERE */;
     retval->data = /* YOUR CODE HERE */;
 
     /* Check the data attribute of our vector to make sure we got memory */
+    /* 检查我们向量的数据属性以确保我们有内存 */
     if (/* YOUR CODE HERE */) {
         free(retval);				//Why is this line necessary?
         allocation_failed();
     }
 
     /* Complete the initialization by setting the single component to zero */
+    /* 通过将单个组件置零来完成初始化 */
     /* YOUR CODE HERE */ = 0;
 
     /* and return... */
@@ -85,9 +97,11 @@ vector_t *vector_new() {
 }
 
 /* Return the value at the specified location/component "loc" of the vector */
+/* 返回向量的指定位置/组件“loc”处的值 */
 int vector_get(vector_t *v, size_t loc) {
 
     /* If we are passed a NULL pointer for our vector, complain about it and exit. */
+    /* 如果我们的 vector 传递了一个 NULL 指针，抱怨它并退出。 */
     if(v == NULL) {
         fprintf(stderr, "vector_get: passed a NULL vector.\n");
         abort();
@@ -96,6 +110,7 @@ int vector_get(vector_t *v, size_t loc) {
     /* If the requested location is higher than we have allocated, return 0.
      * Otherwise, return what is in the passed location.
      */
+    /* 如果请求的位置高于我们分配的位置，则返回 0。否则，返回传递位置中的内容。 */
     if (loc < /* YOUR CODE HERE */) {
         return /* YOUR CODE HERE */;
     } else {
@@ -105,16 +120,19 @@ int vector_get(vector_t *v, size_t loc) {
 
 /* Free up the memory allocated for the passed vector.
    Remember, you need to free up ALL the memory that was allocated. */
+/* 释放为传递的向量分配的内存。请记住，您需要释放所有分配的内存。 */
 void vector_delete(vector_t *v) {
     /* YOUR SOLUTION HERE */
 }
 
 /* Set a value in the vector. If the extra memory allocation fails, call
    allocation_failed(). */
+/* 在向量中设置一个值。 如果额外内存分配失败，调用allocation_failed()。 */
 void vector_set(vector_t *v, size_t loc, int value) {
     /* What do you need to do if the location is greater than the size we have
      * allocated?  Remember that unset locations should contain a value of 0.
      */
+    /* 如果位置大于我们分配的大小，您需要做什么？ 请记住，未设置的位置应包含值 0。 */
 
     /* YOUR SOLUTION HERE */
 }
--- labs/lab02/vector.h
+++ labs/lab02/vector.h
@@ -29,6 +29,25 @@
  * vector.c).  We can get away with doing this even though we don't know what a
  * struct vector is because all struct pointers have the same representation in memory.
  */
+ /*
+ vector.h 最初由 Jeremy Huddleston<jeremyhu@eecs.berkeley.edu> Sp2004 编写
+所以看起来你已经决定冒险进入这个实验室的“其他”文件。 好的。 C 头文件（.h 扩展名）是一种告诉其他
+ .c 文件它们可以访问什么的方式。 您通常在 C 程序中包含 stdlib.h，此过程与包含此 .h 文件相同，
+ 唯一的变化是：
+  #include "file.h"
+ versus
+  #include <file.h>
+  不同之处在于 <> 符号用于系统头文件，而 用于您自己提供的文件（例如在您的本地目录中）。
+  头文件以
+   #ifndef CS61C_VECTOR_H_
+ * #define CS61C_VECTOR_H_
+   开头
+   并以最后的#endif 结尾。 这可以防止文件被多次包含，这可能会导致文件包含的无限循环。
+   首先，我们定义“vector_t”数据类型。 下一行表示“vector_t”与“struct vector_t”相同。
+    因此，在此之后的代码中的任何地方，我们都可以使用“vector_t *”来表示指向“struct vector_t”
+    （在 vector.c 中定义）的指针。 即使我们不知道结构向量是什么，我们也可以这样做，
+    因为所有结构指针在内存中都具有相同的表示形式。
+ */
 
 #include <sys/types.h>
 
@@ -39,17 +58,23 @@ typedef struct vector_t vector_t;
  *  is a way of telling the .c files that #include this header what they will
  *  have access to.
  */
-
+ /*
+ 接下来，我们为 vector.c 中定义的函数提供原型。 这是一种告诉 .c 文件
+ #include this header 他们将有权访问的内容的方法。
+ */
 /* Create a new vector */
 vector_t *vector_new();
 
 /* Free up the memory allocated for the passed vector */
+/* 释放为传递的向量分配的内存 */
 /* YOUR CODE HERE */
+void vector_delete(vector_t *v);
 
 /* Return the value in the vector */
 int vector_get(vector_t *v, size_t loc);
 
 /* Set a value in the vector */
 /* YOUR CODE HERE */
+void vector_set(vector_t *v, size_t loc, int value);
 
 #endif
